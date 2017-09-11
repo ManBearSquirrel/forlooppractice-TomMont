01bca583194029d64871e45e5146064bd86fc16b
tmontgomery <businessmontgomery@gmail.com>
2017-09-11 09:42:54 -0500

    donezo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,84 +7,84 @@ public class Main
     {
         //These loops all produce the wrong output.  Fix them to produce what the preceding output line says.
         System.out.print(" (1) Count 1 to 10: ");
-        for (int i = 1; i <= 5; i++)
-        {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        System.out.print(" (2) Count 0 to 20 by 2s: ");
-        for (int i = 0; i <= 6; i += 2)
-        {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        System.out.print(" (3) Count 0 to 21 by 3s: ");
-        for (int i = 0; i <= 21; i += 2)
-        {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        System.out.print(" (4) Count 5 to 10: ");
         for (int i = 1; i <= 10; i++)
         {
             System.out.print(i + " ");
         }
         System.out.println();
 
+        System.out.print(" (2) Count 0 to 20 by 2s: ");
+        for (int i = 0; i <= 20; i += 2)
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.print(" (3) Count 0 to 21 by 3s: ");
+        for (int i = 0; i <= 21; i += 3)
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.print(" (4) Count 5 to 10: ");
+        for (int i = 5; i <= 10; i++)
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
         System.out.print(" (5) Count 6 to 20 by 2s: ");
-        for (int i = 6; i <= 10; i += 2)
+        for (int i = 6; i <= 20; i += 2)
         {
             System.out.print(i + " ");
         }
         System.out.println();
 
         System.out.print(" (6) Count 6 to 15 by 3s: ");
-        for (int i = 6; i <= 15; i += 2)
+        for (int i = 6; i <= 15; i += 3)
         {
             System.out.print(i + " ");
         }
         System.out.println();
 
         System.out.print(" (7) Count 20 to 1: ");
-        for (int i = 20; i >= 15; i--)
+        for (int i = 20; i >= 1; i--)
         {
             System.out.print(i + " ");
         }
         System.out.println();
 
         System.out.print(" (8) Count 10 to 0 by 2s: ");
-        for (int i = 15; i >= 0; i -= 2)
+        for (int i = 10; i >= 0; i -= 2)
         {
             System.out.print(i + " ");
         }
         System.out.println();
 
         System.out.print(" (9) Count 15 to 0 by 3s: ");
-        for (int i = 15; i >= 0; i -= 2)
+        for (int i = 15; i >= 0; i -= 3)
         {
             System.out.print(i + " ");
         }
         System.out.println();
 
         System.out.print("(10) Count 20 to 5 by 5s: ");
-        for (int i = 25; i >= 0; i -= 5)
+        for (int i = 20; i >= 5; i -= 5)
         {
             System.out.print(i + " ");
         }
         System.out.println();
 
         System.out.print("(11) Count 1 to 5: ");
-        for (int i = 1; i < 10; i++)
+        for (int i = 1; i <= 5; i++)
         {
             System.out.print(i + " ");
         }
         System.out.println();
 
         System.out.print("(12) Count 1 to 10: ");
-        for (int i = 5; i < 7; i++)
+        for (int i = 1; i <= 10; i++)
         {
             System.out.print(i + " ");
         }
